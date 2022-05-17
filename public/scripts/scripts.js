2348c0d84b58ad1e38077201f0f1190ae7a2cf02
Sebastian <sebastiandevelops@gmail.com>
2022-05-17 16:37:57 +0200

    added smooth scrolling effect

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,17 +1,8 @@
-$(document).on("scroll", function() {
-    var pageTop = $(document).scrollTop();
-    var pageBottom = pageTop + $(window).height();
-    var tags = $(".tag");
-  
-    for (var i = 0; i < tags.length; i++) {
-      var tag = tags[i];
-  
-      if ($(tag).position().top < pageBottom) {
-        $(tag).addClass("visible");
-      } else {
-        $(tag).removeClass("visible");
-      }
-    }
-  });
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
 
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
   
